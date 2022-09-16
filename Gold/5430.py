2639bf73b3e9791906a1f87ff08f5479a639163e
DDongYul <comeme0101@naver.com>
2022-09-16 21:40:40 +0900

    문자열 파싱

--- Gold/5430.py
+++ Gold/5430.py
@@ -5,33 +5,33 @@ T = int(sys.stdin.readline())
 for _ in range(0,T):
     p = sys.stdin.readline()            #함수
     n = int(sys.stdin.readline())       #리스트 원소 개수
-    # li = deque(sys.stdin.readline())    #리스트 원소들
-    li = deque(map(int,sys.stdin.readline().replace(',','')[1:-2]))
+    temp_li = sys.stdin.readline().rstrip()[1:-1].split(",")
+    li = deque(temp_li)
     flag = 0 #뒤집힌 상태면 1
+    error = 0
     for i in p:
         if flag == 0:
             if i == 'D' and n == 0:
-                print("error")
+                error = 1
                 break
             elif i =='D' and n!=0:
                 li.popleft()
                 n-=1
             if i =='R':
                 flag = 1
-
         elif flag == 1:
             if i == 'D' and n == 0:
-                print("error")
+                error=1
                 break
             elif i =='D' and n!=0:
                 li.pop()
                 n-=1
             if i =='R':
                 flag = 0
-    if flag == 0:
-        print(list(li))
-    else:
-        result = []
-        for i in range(n-1,0):
-            result.append(i)
-        print(list(result))
+    if error ==1:
+        print("error")
+    elif error == 0 and flag == 0:
+        print("[" + ",".join(li) + "]")
+    elif error == 0 and flag == 1:
+        li.reverse()
+        print("[" + ",".join(li) + "]")
