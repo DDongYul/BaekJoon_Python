ad8114baace4b806de5906f1afd1694763c026e9
DDongYul <comeme0101@naver.com>
2023-01-06 17:52:06 +0900

    벨만포드로 수정

--- Gold/1865.py
+++ Gold/1865.py
@@ -1,35 +1,63 @@
-#도로는 방향 없이 양쪽에 먼저 넣음
-#웜홀을 방향 있게 위에 덮어씀
-#플로이드 와샬
+#첫 풀이:플로이드 와샬 -> 벨만포드로 수정
 
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
+def bellmanFord(start):
+    dist = [INF]*(N+1)
+    dist[start] = 0
+    for i in range(N):
+        for s, d, w in edges:
+           if dist[d] > dist[s]+w:
+                dist[d] = dist[s] + w
+                if i == N-1:
+                    return True
+    return False
+
 TC = int(input())
 for _ in range(TC):
     N,M,W = map(int,input().split())
-    graph = [[INF for _ in range(N)]for _ in range(N)]
+    edges = []
+
     for _ in range(M):
         S,E,T = map(int,input().split())
-        graph[S-1][E-1] = min(graph[S-1][E-1],T)
-        graph[E-1][S-1] = min(graph[E-1][S-1],T)
+        edges.append((S,E,T))
+        edges.append((E,S,T))
+
     for _ in range(W):
-        S, E, T = map(int, input().split())
-        graph[S-1][E-1] = min(graph[S-1][E-1],-T)
+        S,E,T = map(int, input().split())
+        edges.append((S,E,-T))
 
-    flag = False
-    for i in range(N):
-        if flag: break
-        for j in range(N):
-            if flag: break
-            for k in range(N):
-                graph[j][k] = min(graph[j][k],graph[j][i]+graph[i][k])
-                if j == k and graph[j][k]<0:
-                    flag = True
-                    break
-
+    flag = bellmanFord(1)
     if flag:
         print("YES")
     else:
         print("NO")
+
+#########플로이드 와샬###########3
+    # N,M,W = map(int,input().split())
+    # graph = [[INF for _ in range(N)]for _ in range(N)]
+    # for _ in range(M):
+    #     S,E,T = map(int,input().split())
+    #     graph[S-1][E-1] = min(graph[S-1][E-1],T)
+    #     graph[E-1][S-1] = min(graph[E-1][S-1],T)
+    # for _ in range(W):
+    #     S, E, T = map(int, input().split())
+    #     graph[S-1][E-1] = min(graph[S-1][E-1],-T)
+    #
+    # flag = False
+    # for i in range(N):
+    #     if flag: break
+    #     for j in range(N):
+    #         if flag: break
+    #         for k in range(N):
+    #             graph[j][k] = min(graph[j][k],graph[j][i]+graph[i][k])
+    #             if j == k and graph[j][k]<0:
+    #                 flag = True
+    #                 break
+    #
+    # if flag:
+    #     print("YES")
+    # else:
+    #     print("NO")
