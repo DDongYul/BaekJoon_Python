16547fd0a1d8855f1628f6efb3f18f04df8ce6b0
DDongYul <comeme0101@naver.com>
2023-01-13 17:22:10 +0900

    DP로 개선

--- Gold/17070.py
+++ Gold/17070.py
@@ -18,5 +18,21 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int,input().split())))
 
-DFS(0,1,1)
-print(rst)
+dp = [[[0 for _ in range(3)] for _ in range(N)]for _ in range(N)] #dp[row][col][상태] #대각0 가로 1 세로 2
+dp[0][1][1] = 1
+for i in range(2,N):
+    if graph[0][i] == 0:
+        dp[0][i][1] = dp[0][i-1][1]
+
+for i in range(1,N):
+    for j in range(1,N):
+        if not graph[i][j] and not graph[i][j-1] and not graph[i-1][j]:
+            dp[i][j][0] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]
+        if not graph[i][j]:
+            dp[i][j][1] = dp[i][j-1][1]+ dp[i][j-1][0]
+            dp[i][j][2] = dp[i-1][j][2]+ dp[i-1][j][0]
+
+print(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2])
+
+# DFS(0,1,1)
+# print(rst)
