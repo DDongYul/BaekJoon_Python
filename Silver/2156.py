d4bbdf45e5f2ff56a8f05369d4c2281370a649bb
DDongYul <comeme0101@naver.com>
2023-01-24 15:55:23 +0900

    dp -1차원으로 변경

--- Silver/2156.py
+++ Silver/2156.py
@@ -10,22 +10,13 @@ if n == 1:
     print(grape[0])
 elif n == 2:
     print(grape[0]+grape[1])
+elif n == 3:
+    print(max((grape[0] + grape[1]), (grape[0] + grape[2]), (grape[1] + grape[2])))
 else:
-    dp = [[0 for _ in range(2)] for _ in range(n)]      #[0]: 현재 잔이 2번째 잔인 경우 [1]: 현재 잔이 첫번째 잔인 경우
-    dp[0][0] = grape[0]
-    dp[0][1] = grape[0]
-    dp[1][0] = grape[0] + grape[1]
-    dp[1][1] = grape[1]
-
-    for i in range(2,n):
-        mx = 0
-        for j in range(i-3,i-1):
-            mx = max(mx,dp[j][0],dp[j][1])
-        dp[i][1] = mx+grape[i]
-        dp[i][0] = dp[i - 1][1] + grape[i]
-
-    rst = 0
-    for i in dp:
-        rst = max(rst,max(i))
-    print(rst)
-
+    dp = [0 for _ in range(n)]
+    dp[0] = grape[0]
+    dp[1] = grape[0]+grape[1]
+    dp[2] = max((grape[0] + grape[1]), (grape[0] + grape[2]), (grape[1] + grape[2]))
+    for i in range(3,n):
+        dp[i] = max((grape[i] + grape[i-1] + dp[i-3]),(dp[i-2] + grape[i]),(dp[i-1]))
+    print(max(dp))
