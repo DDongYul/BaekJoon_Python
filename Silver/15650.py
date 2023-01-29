1640a7e80366c1e0666cc27ffece8e9540d7c072
DDongYul <comeme0101@naver.com>
2023-01-29 16:52:10 +0900

    조합, itertools 없이 구현

--- Silver/15650.py
+++ Silver/15650.py
@@ -1,8 +1,21 @@
-import itertools
+def comb(arr,n):
+    rst = []
+    if n > len(arr):
+        return rst
+    elif n == 1:
+        for i in arr:
+            rst.append([i])
+    else:
+        for i in range(len(arr)-n+1):
+            for j in comb(arr[i+1:],n-1):
+                rst.append([arr[i]]+j)
+    return rst
+
 N,M = map(int,input().split())
 num = [i for i in range(1,N+1)]
-rst = list(itertools.combinations(num,M))
-for i in rst:
+result = comb(num,M)
+for i in result:
     for j in i:
-        print(j,end=" ")
+        print(j,end=' ')
     print()
+
