f7a9999db40bffc788b0abe6ee37b81d95ff6a4c
DDongYul <comeme0101@naver.com>
2023-02-03 15:26:31 +0900

    트리순회-주석추가

--- Gold/4256.py
+++ Gold/4256.py
@@ -2,26 +2,26 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**5)
 
-def search(pre_start,pre_end,in_start,in_end):
-    if (pre_start>pre_end) or (in_start>in_end):
+def makePost(preStart, preEnd, inStart, inEnd):
+    if (preStart > preEnd) or (inStart > inEnd):
         return
-    root = pre[pre_start]
-    root_index = node_index[root]
-    left_count = root_index-in_start
-    right_count = in_end-root_index
+    root = preorder[preStart]    #preorder의 시작 노드가 루트
+    rootIndex = nodeIndex[root]  #inorder에서 root의 index
+    leftCount = rootIndex - inStart #왼쪽 서브트리 노드의 개수
+    rightCount = inEnd - rootIndex  #오른쪽 서브트리 노드의 개수
 
-    search(pre_start+1,pre_start+left_count,in_start,in_start+left_count-1)
-    search(pre_end-right_count+1,pre_end,in_end-right_count+1,in_end)
+    makePost(preStart + 1, preStart + leftCount, inStart, inStart + leftCount - 1)  #왼쪽 서브트리에 대해 탐색
+    makePost(preEnd - rightCount + 1, preEnd, inEnd - rightCount + 1, inEnd)    #오른쪽 서브트리에 대해 탐색
     print(root)
 
 T = int(input())
 for _ in range(T):
     n = int(input())
-    pre = list(map(int,input().split()))
-    ino = list(map(int, input().split()))
-    node_index = [0 for _ in range(n+1)]
+    preorder = list(map(int, input().split()))
+    inorder = list(map(int, input().split()))
+    nodeIndex = [0 for _ in range(n + 1)]
     for i in range(0,n):
-        node_index[ino[i]]=i
-    search(0,n-1,0,n-1)
+        nodeIndex[inorder[i]]=i
+    makePost(0,n-1,0,n-1)
 
 
